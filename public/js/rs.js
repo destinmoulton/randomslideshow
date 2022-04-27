d87a004270ca8247fd7dd9017c68848ac18a5357
Destin Moulton <destin.moulton@gmail.com>
2022-04-27 10:59:10 -0600

    Added increase and decrease code

--- public/js/rs.js
+++ public/js/rs.js
@@ -2,7 +2,31 @@
  * randomslideshow js
  */
 (function () {
+    let defaultHeight = 200;
     document.addEventListener("DOMContentLoaded", function () {
         // code...
+        const increase = document.getElementById("rs-icon-increase-thumb-size");
+        const decrease = document.getElementById("rs-icon-decrease-thumb-size");
+        increase.addEventListener("click", function (e) {
+            resizeImages("plus");
+        });
+        decrease.addEventListener("click", function (e) {
+            resizeImages("minus");
+        });
     });
+
+    function resizeImages(sizeDir) {
+        console.log("resizeImages called");
+        const thumbs = document.querySelectorAll(".rs-thumb");
+
+        if (sizeDir === "plus") {
+            defaultHeight = defaultHeight + 50;
+        } else {
+            defaultHeight = defaultHeight - 50;
+        }
+        for (let thumb of thumbs) {
+            console.log(thumb);
+            thumb.style.height = defaultHeight + "px";
+        }
+    }
 })();
